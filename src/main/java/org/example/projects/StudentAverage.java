2a61cd956c97bc6fdb4d685cc36f7e1e1e5eb693
Dastan <beginnerstart@mail.ru>
2026-01-31 09:47:37 +0500

    Added some outputs for readability

--- src/main/java/org/example/projects/StudentAverage.java
+++ src/main/java/org/example/projects/StudentAverage.java
@@ -13,6 +13,7 @@ public class StudentAverage {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("How many grades do you have: ");
         int n = scanner.nextInt();
         if(n <= 0) {
             System.out.println("Enter n > 0");
@@ -20,8 +21,10 @@ public class StudentAverage {
         }
         double total = 0;
         for(int i = 0; i < n; i++) {
+            System.out.printf("Enter grade %d: \n", i + 1);
             int number = scanner.nextInt();
             if(number < 0 || number > 100) {
+
                 System.out.println("Enter a number in range of 0-100");
                 return;
             }
